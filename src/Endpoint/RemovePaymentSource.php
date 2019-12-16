a385713e2ea6daa48d2ddf08b09625bf5e50a0a3
Afosto <ci-bot@afosto.io>
2019-12-16 19:07:54 +0000

    CI commit

--- src/Endpoint/RemovePaymentSource.php
+++ src/Endpoint/RemovePaymentSource.php
@@ -31,7 +31,7 @@ class RemovePaymentSource extends \Jane\OpenApiRuntime\Client\BaseEndpoint imple
 
     public function getUri(): string
     {
-        return str_replace(['{id}'], [$this->id], '/iam/cards/{id}');
+        return str_replace(['{id}'], [$this->id], '/iam/sources/{id}');
     }
 
     public function getBody(\Symfony\Component\Serializer\SerializerInterface $serializer, $streamFactory = null): array
